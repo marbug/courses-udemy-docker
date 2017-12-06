fbbc98c2cc11669fe4f5e87ae25cb56db26da9be
Maryan Bahnyuk <marbuga@gmail.com>
2017-12-06 16:07:30 +0200

    add scripts

---
+++ scripts/use-bitbucket.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git remote -v
+git remote set-url origin git@bitbucket.org:marbug/courses-udemy-docker.git
+git remote -v
---
+++ scripts/use-ququdev.bat
@@ -0,0 +1,3 @@
+git remote -v
+git remote set-url origin marbug@ququdev.com:/shared/git/marbug/courses-udemy-docker.git
+git remote -v
